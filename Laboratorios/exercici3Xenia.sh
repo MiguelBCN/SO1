157e3d01a5ed2ee7065c50dc4357c3ed3d9ecd64
Xènia Domènech <xeniadomenech@MacBook-Air-de-Xenia.local>
2021-02-23 19:25:52 +0100

    Exercici 3

---
+++ Laboratorios/exercici3Xenia.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+#Funció recursiva
+
+obrimfitxers()
+{
+
+	paraula=$3
+	for f in $myfiles
+	do
+		if [[ ! -d $f && ! -f $f ]]; then
+			echo "Error en la lectura dels fitxers o dels directoris."
+			return 1
+		fi
+
+		#Comprovo si tinc un fitxer amb l'extensió necessària
+
+		if [[ -f $f && ${f#*.} == $2 ]]; then
+			
+			#Comprovo si el fitxer té la paraula demanada
+
+			paraulesdelfitxer "$f" "$paraula"
+		fi
+		
+		#Comprovo si tinc un subdirectori
+
+		if [ -d $f ]; then
+
+			#Em guardo els fitxers del subdirectori en "myfiles"
+
+			myfiles=$(ls "$f")
+
+			#Entro dins del subdirectori
+
+			cd ./$f
+
+			#Faig recursivitat
+
+			obrimfitxers "$myfiles" "$2" "$paraula"
+
+			#Torno al directori anterior
+
+			cd ..
+		fi
+	done
+	return 0
+
+}
+
+		
+paraulesdelfitxer()
+{
+	if [ -f $f ]; then
+		text=$(cat $f)
+		for i in $text
+		do
+			if [[ $i == $paraula ]]; then
+				echo "$f: $i"
+				return 0
+			fi
+		done
+	fi
+}
+
+#Funció main
+
+cd "$1"
+
+if [[ ! -d $1 || -f $2 || -z $3 ]]; then
+	echo "Error en l'entrada"
+	exit 1
+
+	else
+		myfiles=$(ls "$1")
+		obrimfitxers "$myfiles" "$2" "$3"
+		exit 0
+fi
+
