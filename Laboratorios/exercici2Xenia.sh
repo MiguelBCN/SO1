a75b3981aba4228e7b2921c6743829d5c9f332e3
Xènia Domènech <74207054+xeniadomenech@users.noreply.github.com>
2021-02-27 11:54:10 +0100

    Add files via upload

--- Laboratorios/exercici2Xenia.sh
+++ Laboratorios/exercici2Xenia.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-cd "$1"
+if [ $# -ne 2 ]
+then
+        echo "El nombre de parametres introduits es incorrecte"
+        exit 1
+fi
 
 if [[ ! -d $1 || -z $2 ]]; then
 	echo "Error en l'entrada"
 	exit 1
 fi
 
+cd "$1"
 myfiles=$(ls "$1")
+apareix=0
 for f in $myfiles 
 do 
 	if [ -f $f ]; then
@@ -16,8 +22,13 @@ do
 		do
 			if [[ "$i" == *"$2"* ]]; then
 				echo "$f: $i"
+				apareix=1
 			fi
 		done
 	fi
 done
+
+if [[ $apareix -eq 0 ]]; then
+	echo "La cadena $2 no apareix en cap fitxer d'aquest directori"
+fi
 exit 0;
