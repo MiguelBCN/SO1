efc1141dc7420bdebda9e83422dc1e2c0b56dc95
Xènia Domènech <74207054+xeniadomenech@users.noreply.github.com>
2021-03-03 17:04:36 +0100

    Update exercici1.sh

--- Laboratorios/exercici1.sh
+++ Laboratorios/exercici1.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+#Comprovació del nombre de paràmetres
 if [ $# -ne 1 ]
 then
 	echo "El nombre de parametres introduits es incorrecte."
 	exit 1
 fi
 
+#Comprovació del tipus de paràmetres
 if [ ! -f $1 ]; then
 	echo "Error: no ha introduit un fitxer o aquest no existeix."
 	exit 1
 fi
 
+#Comprovo quines lletres de $1 són caràcters especials i les substitueixo per un guió
 nom=$1
 for (( i = 0; i < ${#nom}; i++ ))
 do
