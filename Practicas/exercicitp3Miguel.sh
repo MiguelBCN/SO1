e5f5563656cfee1c16059bbd323bf53595694c6c
Miguel Huayllas <huayllasmiguel@gmail.com>
2021-03-10 02:06:06 +0100

    Create exercicitp3Miguel.sh

---
+++ Practicas/exercicitp3Miguel.sh
@@ -0,0 +1,18 @@
+# Buscar en un directorio el numero de ficheros de cada suibdirectorio
+# Comprobaciones
+if [ $# -ne 1 ];then
+	echo "Se necesita solo un parametro"
+	exit 1
+fi
+if [ ! -d $1 ];then
+	echo "El parametro solo puede ser un directorio"
+	exit 1
+fi
+
+directorios=$(find "$1" -type d)
+for i in $directorios
+do
+	peso=$(ls "$i" | wc -l)
+	echo "directori: $i nfiles: $peso"
+done
+exit 0
