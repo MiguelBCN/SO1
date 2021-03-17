f270a80804576e9a5f57708e94125c30571a80a4
Miguel Huayllas <huayllasmiguel@gmail.com>
2021-03-17 19:01:32 +0100

    Create exercici6.sh

---
+++ Laboratorios/P2_Grup44_Miguel_Huayllas_Xenia_Domenech/exercici6.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+# Script que recibe como parametro un nombre de usuario y
+# una cadena ya sea VSZ o RSS
+#  Mostar los 10 primeros procesos ordenados de forma descendente por VSZ o RSS
+
+# Comprabaciones
+if [ $#  -ne 2 ];then
+	echo "Numero de parametros erroneo se espera un nombre de usuario y (VSZ o RSS)"
+	exit 1
+fi
+if [ -d $1  -o  -f $1 ];then
+	echo "El primer parametro solo puede ser una nombre no poner archivos ni directorios"
+	exit 1
+fi
+
+# Imprimir si el usuario no tiene procesos bajo su nombre
+ps -aux | grep "^$1\b"
+if [ $? -eq 1 ];then
+	echo "El usuario $1 no tiene ningun proceso adjuntado"
+	exit 1
+fi
+
+# Programa
+if [ $2 == "VSZ" ];then
+	ps -aux | grep "^$1\b" | awk '{print $5 " "$6 " " $11}' | sort --numeric-sort -k1 |head
+else
+	ps -aux | grep "^$1\b" | awk '{print $5 " "$6 " " $11}' | sort --numeric-sort -k2 |head
+fi
+
+exit 0
