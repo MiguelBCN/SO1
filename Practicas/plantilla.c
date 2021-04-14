41a313be59a3789e87ba5153a342d68570a23424
Miguel Huayllas <huayllasmiguel@gmail.com>
2021-04-14 23:35:44 +0200

    Update plantilla.c

--- Practicas/plantilla.c
+++ Practicas/plantilla.c
@@ -6,6 +6,7 @@
 #include <sys/wait.h>
 
 #define MAXVALUE 1000
+#define NUMVALUES 100
 
 void sigusr1(int signo)
 {
@@ -28,27 +29,26 @@ int main(void)
     ret = fork();
 
     if (ret == 0) { // child
-      
+
         /* Inserir codi aqui per gestionar senyals */
         signal(SIGUSR2, sigusr2);
         parent_pid = getppid();
-       
+
         /* Esperar a rebre senyal del pare */
         pause();
-        sleep(5);
+        
 
         /* Llegir el nombre de valors que ens envia el pare */
         i = 0;
         sum = 0;
+        num_values=NUMVALUES; // Sin esta linea el valor de num_values serai algo como 78635616 
         while (i < num_values) {
             /* Llegir valors de la canonada i fer la suma */
             read(fd[0], &value,4);
-            
             sum = sum+value;
-            printf("El valor acutal de la suma es %d y del value %d\n",sum,value);
             i++;
         }
-    
+
         printf("El fill escriu el resultat: %d \n", sum);
         /* Escriure el valor de la suma a la canonada */
         write(fd[1], &sum, 4);
@@ -67,30 +67,29 @@ int main(void)
         srand(time(NULL));
 
         i = 0;
-       
+
         /* Escriure aquest valor a la canonada */
-        num_values = 10;
+        num_values = NUMVALUES;
         
         while (i < num_values) {  
             /* Generar valor aleatori i inserir a la canonada */
             value = rand() % MAXVALUE + 1;
             write(fd[1], &value,4);
-             printf("El ultimo valor escrito es %d\n", value);
             i++;
         }
         
-       
-
-
-
         printf("El pare espera la suma...\n");
 
         /* Inserir codi aqui per esperar senyal del pare */
-                sleep(5);
-                kill(child_pid,SIGUSR2);
-                pause();
+        sleep(5);
+        kill(child_pid,SIGUSR2);
+        pause();
 
         /* Llegir el resultat */
+        while (i < num_values) { 
+            read(fd[0], &sum, 4);
+            i++;
+        }
         read(fd[0], &sum, 4);
 
         printf("El fill em diu que la suma es: %d\n", sum);
